48fc28ecfda83f1320301309a862e369bbb4de54
Urban Hafner <contact@urbanhafner.com>
2017-04-24 08:05:51 +0200

    Add pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/pages/:id" => "pages#show"
+  root "pages#show", id: "home"
 end
