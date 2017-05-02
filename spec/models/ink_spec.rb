7dc47fc8b4d290bfeb304b0fcd95bf2314f290a1
Urban Hafner <contact@urbanhafner.com>
2017-05-02 16:29:22 +0200

    Ink specs

---
+++ spec/models/ink_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe Ink do
+  it 'requires a name' do
+    expect(subject).to_not be_valid
+    expect(subject.errors).to include(:name)
+  end
+
+  it 'requires a manufacturer' do
+    expect(subject).to_not be_valid
+    expect(subject.errors).to include(:manufacturer)
+  end
+end
