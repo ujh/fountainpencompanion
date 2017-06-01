7bafea7e8dd5e2e95bbd8acd3a5f7a7ab4402e4f
Urban Hafner <contact@urbanhafner.com>
2017-06-01 08:09:57 +0200

    Display profile image in the header

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -1,12 +1,12 @@
 module AccountsHelper
 
-  def profile_image(user)
-    image_tag(profile_image_url(user))
+  def profile_image(size: 500)
+    image_tag(profile_image_url(size: size))
   end
 
-  def profile_image_url(user)
-    hash = Digest::MD5.hexdigest(user.email.downcase)
-    "https://www.gravatar.com/avatar/#{hash}.jpg?s=500"
+  def profile_image_url(size:)
+    hash = Digest::MD5.hexdigest(current_user.email.downcase)
+    "https://www.gravatar.com/avatar/#{hash}.jpg?s=#{size}"
   end
 
 end
