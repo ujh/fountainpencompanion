520133df45fa51629a1f7076a3c526cbd31b155e
Urban Hafner <contact@urbanhafner.com>
2017-06-20 08:01:08 +0200

    Redirect to inks page after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,8 @@ class ApplicationController < ActionController::Base
   rescue_from ActionView::MissingTemplate do |exception|
     render file: "public/404.html", status: :not_found, layout: false
   end
+
+  def after_sign_in_path_for(resource)
+    collected_inks_path
+  end
 end
