100221b0c16cf99c646c4d6745ad9f1409ac5e61
Urban Hafner <contact@urbanhafner.com>
2017-06-27 13:23:41 +0200

    Latest dependencies & make it work for one user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def index
     rel = user_signed_in? ? User.where("id <> ?", current_user.id) : User
-    count = rel.count
+    count = [rel.count, 1].max
     @users = rel.order('name').in_groups_of((count.to_f / 3).ceil, false)
   end
 
