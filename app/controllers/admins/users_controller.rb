75903fc14925c23b31e6519af0d273cd84d272b0
Urban Hafner <contact@urbanhafner.com>
2017-07-04 08:50:05 +0200

    CSV import

--- app/controllers/admins/users_controller.rb
+++ app/controllers/admins/users_controller.rb
@@ -3,14 +3,32 @@ class Admins::UsersController < ApplicationController
   layout 'admin'
 
   before_action :authenticate_admin!
+  before_action :fetch_user, only: [:become, :import]
 
   def index
     @users = User.all
   end
 
   def become
-    user = User.find(params[:id])
-    sign_in(:user, user, bypass: true)
+    sign_in(:user, @user, bypass: true)
     redirect_to root_url
+  end
+
+  def import
+    count = 0
+    CSV.parse(params[:file].read, headers: true) do |row|
+      row = row.to_hash
+      row["private"] = !row["private"].blank?
+      row.keys.each {|k| row[k] = '' if row[k].nil?}
+      count +=1 if @user.collected_inks.create(row)
+    end
+    flash[:notice] = "#{count} inks imported for #{@user.email}"
+    redirect_to admins_users_path
+  end
+
+  private
+
+  def fetch_user
+    @user = User.find(params[:id])
   end
 end
