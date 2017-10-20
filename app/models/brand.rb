0c43b3dfd3565eb78dbec8dd900b6c199b609a74
Urban Hafner <contact@urbanhafner.com>
2017-10-20 08:05:39 +0200

    Use brands view in the autocomplete

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,2 +1,7 @@
 class Brand < ApplicationRecord
+
+  def self.search(term)
+    simplified_term = Simplifier.simplify(term)
+    where("simplified_brand_name LIKE ?", "%#{simplified_term}%").order(:popular_name)
+  end
 end
