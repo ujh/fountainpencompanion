347d6fc591bd9e13f0e3dceb048088c33bb8c4f8
Urban Hafner <contact@urbanhafner.com>
2018-01-30 17:01:46 +0100

    Button with nice design

--- app/javascript/collected_inks/index.jsx
+++ app/javascript/collected_inks/index.jsx
@@ -4,7 +4,7 @@ import { ChromePicker } from "react-color";
 
 export function renderColorPickerApp(element) {
   let input = $(element).find('input');
-  let initialColor = input.val() || "#fff";
+  let initialColor = input.val() || "#000";
   ReactDOM.render(<App color={initialColor} />, element);
 }
 
@@ -20,11 +20,30 @@ class App extends React.Component {
 
   render() {
     let color = this.state.color;
-    return <div style={{backgroundColor: color}}>
-      {color}
+    return <div>
+      <Button color={color}/>
       <ColorInputField color={color} />
     </div>;
   }
+}
+
+const Button = ({color}) => {
+  let outerCSS = {
+    padding: '5px',
+    background: '#fff',
+    borderRadius: '1px',
+    boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+    display: 'inline-block',
+    cursor: 'pointer',
+  }
+  let innerCSS = {
+    width: '36px',
+    height: '23px',
+    borderRadius: '2px',
+    boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
+    backgroundColor: color,
+  };
+  return <div style={outerCSS}><div style={innerCSS}></div></div>;
 }
 
 const ColorInputField = ({color}) => {
