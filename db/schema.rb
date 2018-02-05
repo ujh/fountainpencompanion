a3f9af86da88cb8ac18b755fd7523b05254dd96a
Urban Hafner <contact@urbanhafner.com>
2018-02-05 08:15:57 +0100

    Add comment to pen

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180205070500) do
+ActiveRecord::Schema.define(version: 20180205071528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 20180205070500) do
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "comment"
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
