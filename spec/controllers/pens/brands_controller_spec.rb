373d69bb6c56bbefde55881de35baed56a0e0785
Urban Hafner <contact@urbanhafner.com>
2018-02-06 08:54:31 +0100

    Specs for the pen brand autocomplete

--- spec/controllers/pens/brands_controller_spec.rb
+++ spec/controllers/pens/brands_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe Pens::BrandsController, type: :controller do
+describe Pens::BrandsController do
+  fixtures :collected_pens
 
+  describe '#index' do
+
+    it 'returns all brands with an empty search term' do
+      get :index, params: { term: ''}, format: :json
+      expect(response).to be_successful
+      expect(JSON.parse(response.body)).to eq(["Pilot", "Platinum", "Wing Sung"])
+    end
+
+    it 'returns brands by substring search' do
+      get :index, params: { term: 'P'}, format: :json
+      expect(response).to be_successful
+      expect(JSON.parse(response.body)).to eq(["Pilot", "Platinum"])
+    end
+  end
 end
