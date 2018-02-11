dc2e5e8f7199f47058ef7f459134c796a2f7649a
Urban Hafner <contact@urbanhafner.com>
2018-02-11 13:54:14 +0100

    Name delegates for ink and pen

--- app/models/currently_inked.rb
+++ app/models/currently_inked.rb
@@ -3,4 +3,7 @@ class CurrentlyInked < ApplicationRecord
   belongs_to :collected_ink
   belongs_to :collected_pen
 
+  delegate :name, to: :collected_ink, prefix: true
+  delegate :name, to: :collected_pen, prefix: true
+
 end
