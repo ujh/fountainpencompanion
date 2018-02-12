b25e8cefc7921b1c92e323ee1b98369685b767f9
Urban Hafner <contact@urbanhafner.com>
2018-02-12 12:42:51 +0100

    Validate that pen and ink belong to user

--- app/models/currently_inked.rb
+++ app/models/currently_inked.rb
@@ -7,4 +7,19 @@ class CurrentlyInked < ApplicationRecord
   delegate :name, to: :collected_ink, prefix: 'ink', allow_nil: true
   delegate :name, to: :collected_pen, prefix: 'pen', allow_nil: true
 
+  validate :collected_ink_belongs_to_user
+  validate :collected_pen_belongs_to_user
+
+  private
+
+  def collected_ink_belongs_to_user
+    return unless user_id && collected_ink
+    errors.add(:collected_ink) if user_id != collected_ink.user_id
+  end
+
+  def collected_pen_belongs_to_user
+    return unless user_id && collected_pen
+    errors.add(:collected_pen) if user_id != collected_pen.user_id
+  end
+
 end
--- spec/models/currently_inked_spec.rb
+++ spec/models/currently_inked_spec.rb
@@ -1,4 +1,41 @@
 require 'rails_helper'
 
 describe CurrentlyInked do
+
+  fixtures :users, :collected_inks, :collected_pens
+
+  describe 'validations' do
+
+    let(:user) { users(:moni) }
+
+    before(:each) do
+      subject.user = users(:moni)
+    end
+
+    it 'fails if the ink belongs to another user' do
+      subject.collected_ink = collected_inks(:toms_marine)
+      expect(subject).to be_invalid
+      expect(subject.errors).to include(:collected_ink)
+    end
+
+    it 'validates if the ink belongs to the same user' do
+      subject.collected_ink = collected_inks(:monis_marine)
+      expect(subject).to be_invalid
+      expect(subject.errors).to_not include(:collected_ink)
+    end
+
+    it 'fails if the pen belongs to another user' do
+      subject.collected_pen = collected_pens(:toms_platinum)
+      expect(subject).to be_invalid
+      expect(subject.errors).to include(:collected_pen)
+    end
+
+    it 'validates if the pen belongs to the same user' do
+      subject.collected_pen = collected_pens(:monis_wing_sung)
+      expect(subject).to be_invalid
+      expect(subject.errors).to_not include(:collected_pen)
+    end
+
+  end
+
 end
