43223690147ac5d7de6815d5c9483614040bfb23
Urban Hafner <contact@urbanhafner.com>
2018-02-18 15:49:01 +0100

    Date picker for currently inked

--- app/assets/javascripts/currently_inked.js
+++ app/assets/javascripts/currently_inked.js
@@ -1,3 +1,7 @@
 $(function() {
   $('#currently-inked, #currently-inked-archive').find('select').select2();
+
+  $('#currently_inked_inked_on, #currently_inked_archived_on').datepicker({
+    dateFormat: 'yy-mm-dd'
+  });
 })
