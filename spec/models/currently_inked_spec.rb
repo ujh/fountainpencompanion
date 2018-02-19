c6cb7cf97339c2b3baabb0c9ab73af6403ec2382
Urban Hafner <contact@urbanhafner.com>
2018-02-19 19:46:59 +0100

    More tests

--- spec/models/currently_inked_spec.rb
+++ spec/models/currently_inked_spec.rb
@@ -4,13 +4,13 @@ describe CurrentlyInked do
 
   fixtures :users, :collected_inks, :collected_pens
 
+  let(:user) { users(:moni) }
+
+  before(:each) do
+    subject.user = users(:moni)
+  end
+
   describe 'validations' do
-
-    let(:user) { users(:moni) }
-
-    before(:each) do
-      subject.user = users(:moni)
-    end
 
     it 'fails if the ink belongs to another user' do
       subject.collected_ink = collected_inks(:toms_marine)
@@ -36,6 +36,77 @@ describe CurrentlyInked do
       expect(subject.errors).to_not include(:collected_pen)
     end
 
+    it 'fails if the pen is already in use' do
+      pen = collected_pens(:monis_wing_sung)
+      user.currently_inkeds.create!(
+        collected_pen: pen,
+        collected_ink: collected_inks(:monis_marine)
+      )
+      subject.collected_pen = pen
+      expect(subject).to be_invalid
+      expect(subject.errors).to include(:collected_pen_id)
+    end
+
+    it 'validates if the pen is only in an archived entry' do
+      pen = collected_pens(:monis_wing_sung)
+      user.currently_inkeds.create!(
+        collected_pen: pen,
+        collected_ink: collected_inks(:monis_marine),
+        archived_on: Date.today
+      )
+      subject.collected_pen = pen
+      expect(subject).to be_invalid
+      expect(subject.errors).to_not include(:collected_pen)
+    end
   end
 
+  describe '#initialize' do
+    it 'sets a default inked_on' do
+      expect(subject.inked_on).to eq(Date.today)
+    end
+
+    it 'does not override an existing inked_on' do
+      date = Date.yesterday
+      ci = CurrentlyInked.new(inked_on: date)
+      expect(ci.inked_on).to eq(date)
+    end
+  end
+
+  describe '#unarchivable?' do
+
+    let(:pen) { collected_pens(:monis_wing_sung) }
+
+    before(:each) do
+      subject.update!(
+        collected_pen: pen,
+        collected_ink: collected_inks(:monis_fire_and_ice),
+        archived_on: Date.today
+      )
+    end
+
+    it 'returns false if there is an active currently inked with that pen' do
+      user.currently_inkeds.create!(
+        collected_pen: pen,
+        collected_ink: collected_inks(:monis_marine)
+      )
+      expect(subject).to_not be_unarchivable
+    end
+
+    it 'returns true if there is an archived entry with that pen' do
+      user.currently_inkeds.create!(
+        collected_pen: pen,
+        collected_ink: collected_inks(:monis_marine),
+        archived_on: Date.today
+      )
+      expect(subject).to be_unarchivable
+    end
+  end
+
+  describe '#collected_pens_for_active_select' do
+
+  end
+
+  describe '#collected_inks_for_active_select' do
+
+  end
 end
