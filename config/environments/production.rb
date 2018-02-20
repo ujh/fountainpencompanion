06fd52505483351ea3a75b2f8bd8a40536c49a08
Urban Hafner <contact@urbanhafner.com>
2018-02-20 12:23:38 +0100

    Use exception notifier

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,4 +98,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.middleware.use(
+    ExceptionNotification::Rack,
+    email: {
+      :email_prefix => "[FPC] ",
+      :sender_address => %{"Exception Notifier" <exception-notifier@fountainpencompanion.com>},
+      :exception_recipients => %w{contact@urbanhafner.com}
+    }
+  )
 end
