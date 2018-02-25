37bcfcb3fb84086d80aaf610f19a19c24da57406
Urban Hafner <contact@urbanhafner.com>
2018-02-25 17:16:05 +0100

    Add polyfills

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,17 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+// Needs to come first. http://babeljs.io/docs/usage/polyfill/
+import "babel-polyfill";
+// Promise polyfill. https://github.com/taylorhakes/promise-polyfill
+import { Promise } from "promise-polyfill";
+// To add to window
+if (!window.Promise) {
+  window.Promise = Promise;
+}
+// fetch polyfill. https://github.com/github/fetch
+import "whatwg-fetch";
+
 import { renderColorPickerApp } from "../src/collected_inks";
 
 window.renderColorPickerApp = renderColorPickerApp;
