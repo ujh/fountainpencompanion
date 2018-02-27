f92a4a0345738755ca7c2cda9f8b5890652601f5
Urban Hafner <contact@urbanhafner.com>
2018-02-27 19:01:31 +0100

    Correct inks autocomplate caching

--- app/controllers/inks_controller.rb
+++ app/controllers/inks_controller.rb
@@ -1,7 +1,7 @@
 class InksController < ApplicationController
 
   def index
-    inks = Rails.cache.fetch('inks', expires_in: 5.minutes) do
+    inks = Rails.cache.fetch("inks_#{params[:term]}", expires_in: 5.minutes) do
       Ink.search(params[:term]).pluck(:popular_ink_name)
     end
     render json: inks
