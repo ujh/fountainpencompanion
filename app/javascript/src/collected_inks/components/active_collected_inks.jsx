9af1c42f6fa1b49fae0acdcc15e9a010a3a2048d
Urban Hafner <contact@urbanhafner.com>
2018-02-28 17:30:44 +0100

    Display tables with inks

--- app/javascript/src/collected_inks/components/active_collected_inks.jsx
+++ app/javascript/src/collected_inks/components/active_collected_inks.jsx
@@ -1,7 +1,10 @@
 import * as React from "react";
 import { connect } from "react-redux";
 
+import InkTable from "./ink_table";
+
 const mapStateToProps = ({ active }) => ({ entries: active });
 
-const ActiveCollectedInks = ({entries}) => <div>{entries.length} active collected inks</div>
+const ActiveCollectedInks = ({entries}) => <div><InkTable entries={entries}/></div>;
+
 export default connect(mapStateToProps)(ActiveCollectedInks);
--- app/javascript/src/collected_inks/components/archived_collected_inks.jsx
+++ app/javascript/src/collected_inks/components/archived_collected_inks.jsx
@@ -1,7 +1,10 @@
 import * as React from "react";
 import { connect } from "react-redux";
 
+import InkTable from "./ink_table";
+
 const mapStateToProps = ({ archived }) => ({ entries: archived });
 
-const ArchivedCollectedInks = ({entries}) => <div>{entries.length} archived collected inks</div>
+const ArchivedCollectedInks = ({entries}) => <div><InkTable entries={entries} /></div>;
+
 export default connect(mapStateToProps)(ArchivedCollectedInks);
