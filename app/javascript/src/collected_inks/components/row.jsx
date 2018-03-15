cfb10393da29546c6239520ae47cdfe4ed83252c
Urban Hafner <contact@urbanhafner.com>
2018-03-15 08:29:38 +0100

    Separate component for Privacy

--- app/javascript/src/collected_inks/components/row.jsx
+++ app/javascript/src/collected_inks/components/row.jsx
@@ -3,24 +3,37 @@ import { connect } from "react-redux";
 
 import { deleteEntry } from "src/collected_inks/actions";
 
-const Row = (props) => <tr className={`${props.private ? "private" : ""}`}>
-  <td><i className={`fa fa-${props.private ? "lock" : "unlock"}`}></i></td>
-  <td>{props.brand_name}</td>
-  <td>{props.line_name}</td>
-  <td>{props.ink_name}</td>
-  <td>{props.kind}</td>
-  <td style={{backgroundColor: props.color}}></td>
-  <td className={`swabbed ${props.swabbed}`}><i className={`fa fa-${props.swabbed ? "check" : "times"}`}></i></td>
-  <td className={`used ${props.used}`}><i className={`fa fa-${props.used ? "check" : "times"}`}></i></td>
-  <td>{props.comment}</td>
-  <td>
-    <span className="actions">
-      <a className="btn btn-default"><i className="fa fa-archive" /></a>
-      <DeleteButton deletable={props.deletable} onClick={props.onDelete}/>
-    </span>
-  </td>
-</tr>;
+class Row extends React.Component {
+  render() {
+    const props = this.props;
+    return <tr className={this.className()}>
+      <td><Privacy private={props.private} /></td>
+      <td>{props.brand_name}</td>
+      <td>{props.line_name}</td>
+      <td>{props.ink_name}</td>
+      <td>{props.kind}</td>
+      <td style={{backgroundColor: props.color}}></td>
+      <td className={`swabbed ${props.swabbed}`}><i className={`fa fa-${props.swabbed ? "check" : "times"}`}></i></td>
+      <td className={`used ${props.used}`}><i className={`fa fa-${props.used ? "check" : "times"}`}></i></td>
+      <td>{props.comment}</td>
+      <td>
+        <span className="actions">
+          <a className="btn btn-default"><i className="fa fa-archive" /></a>
+          <DeleteButton deletable={props.deletable} onClick={props.onDelete}/>
+        </span>
+      </td>
+    </tr>;
+  }
 
+  className() {
+    return (this.props.private ? "private" : "");
+  }
+}
+
+const Privacy = (props) => {
+  const icon = props.private ? "lock" : "unlock";
+  return <i className={`fa fa-${icon}`}></i>;
+}
 const DeleteButton = ({deletable, onClick}) => {
   if (!deletable) return null;
   return <a onClick={onClick} className="btn btn-default"><i className="fa fa-trash" /></a>;
