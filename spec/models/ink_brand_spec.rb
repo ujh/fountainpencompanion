f9d50e42e564ad770365e51d87ec8095b02b115d
Urban Hafner <contact@urbanhafner.com>
2018-07-31 07:55:35 +0200

    Only show public and non-empty ink brands

--- spec/models/ink_brand_spec.rb
+++ spec/models/ink_brand_spec.rb
@@ -14,4 +14,30 @@ describe InkBrand do
       expect(subject.popular_name).to eq('Two')
     end
   end
+
+  describe '#public' do
+
+    let!(:ink_brand) { InkBrand.create! }
+
+    it 'does not return InkBrands without attached collected inks' do
+      expect(InkBrand.count).to eq(1)
+      expect(InkBrand.public.count).to eq(0)
+    end
+
+    it 'does not return InkBrands with only private collected inks' do
+      create(:collected_ink, ink_brand: ink_brand, private: true)
+      expect(InkBrand.public.count).to eq(0)
+    end
+
+    it 'returns InkBrands with only public collected inks' do
+      create(:collected_ink, ink_brand: ink_brand, private: false)
+      expect(InkBrand.public.count).to eq(1)
+    end
+
+    it 'returns InkBrands with collected inks of mixed privacy' do
+      create(:collected_ink, ink_brand: ink_brand, private: false)
+      create(:collected_ink, ink_brand: ink_brand, private: true)
+      expect(InkBrand.public.count).to eq(1)
+    end
+  end
 end
