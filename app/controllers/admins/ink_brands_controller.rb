2691dd9998072312100b88f98726129467e2d983
Urban Hafner <contact@urbanhafner.com>
2018-09-14 16:56:36 +0200

    Display number of inks per brand

--- app/controllers/admins/ink_brands_controller.rb
+++ app/controllers/admins/ink_brands_controller.rb
@@ -1,6 +1,6 @@
 class Admins::InkBrandsController < Admins::BaseController
 
   def index
-    @ink_brands = InkBrand.public.order("ink_brands.popular_name ASC").includes(:collected_inks)
+    @ink_brands = InkBrand.public.order("ink_brands.popular_name ASC").includes(:collected_inks, :new_ink_names)
   end
 end
