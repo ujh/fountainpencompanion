24aca1d13627264ca12992930b69249018c258b7
Urban Hafner <contact@urbanhafner.com>
2018-09-15 21:18:45 +0200

    Order by line name and then ink name

--- app/controllers/admins/ink_brands_controller.rb
+++ app/controllers/admins/ink_brands_controller.rb
@@ -7,6 +7,6 @@ class Admins::InkBrandsController < Admins::BaseController
   def show
     @ink_brand = InkBrand.find(params[:id])
     @new_ink_names = NewInkName.where(ink_brand_id: params[:id])
-      .order("new_ink_names.popular_name ASC").includes(:collected_inks)
+      .order("new_ink_names.popular_line_name, new_ink_names.popular_name ASC").includes(:collected_inks)
   end
 end
