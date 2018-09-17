87adeaea5588f11d3e3a7fd57b1d4923bf5be336
Urban Hafner <contact@urbanhafner.com>
2018-09-17 07:34:10 +0200

    Extract code for color transferal into a new class

--- app/operations/save_collected_ink.rb
+++ app/operations/save_collected_ink.rb
@@ -28,34 +28,6 @@ class SaveCollectedInk
   end
 
   def update_color_of_unset_twins!
-    unset_twins.update_all(color: average_color) if twins_with_color.exists?
-  end
-
-  def unset_twins
-    twins.without_color
-  end
-
-  def twins
-    collected_ink.twins
-  end
-
-  def average_color
-    Color.new([:red, :green, :blue].map {|f| average_for(colors_of_twins, f)}).html
-  end
-
-  def colors_of_twins
-    @colors ||= twins_with_color.pluck(:color).map do |c|
-      Color::RGB.from_html(c)
-    end
-  end
-
-  def twins_with_color
-    twins.with_color
-  end
-
-  def average_for(colors, field)
-    sum = (colors.map {|c| c.send(field)**2}.sum)
-    size = colors.size.to_f
-    Math.sqrt(sum/size).round
+    UpdateColor.new(collected_ink).perform
   end
 end
