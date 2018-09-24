5e8caecf334a7b23782651b2914e37d75ec85dbf
Urban Hafner <contact@urbanhafner.com>
2018-09-24 13:11:56 +0200

    Get data of logged in user and check if it's the same user or not

--- app/javascript/src/public_inks/app.jsx
+++ app/javascript/src/public_inks/app.jsx
@@ -5,20 +5,39 @@ import Table from "./table";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-     this.state = { userData: [] }
+     this.state = {
+       userData: { user_id: null, inks: [] },
+       loggedInUserData: { user_id: null, inks: [] }
+      }
   }
 
   componentDidMount() {
     getUserData(data => this.setState({userData: data}))
+    getloggedInUserData(data => this.setState({loggedInUserData: data}))
   }
 
   tableData() {
-    return this.state.userData;
+    let ld = this.state.loggedInUserData;
+    let ud = this.state.userData;
+    if (ld.user_id && ld.user_id != ud.user_id) {
+      console.log('additional data')
+    }
+    return ud.inks;
   }
 
   render() {
     return <Table data={this.tableData()} />
   }
+}
+
+function getloggedInUserData(callback) {
+  getRequest("/account").then(
+    response => {
+      if (response.ok) {
+        response.json().then(json => callback(processData(json)))
+      }
+    }
+  )
 }
 
 function getUserData(callback) {
@@ -33,5 +52,5 @@ function processData(data) {
   let processedData = data.included.filter(
     e => e.type == 'collected_inks'
   ).map(e => e.attributes);
-  return processedData;
+  return { user_id: data.data.id, inks: processedData};
 }
