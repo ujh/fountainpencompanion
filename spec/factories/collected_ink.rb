72fbd969062d9581f38e2da4c435ec9b25f608ff
Urban Hafner <contact@urbanhafner.com>
2018-09-24 07:13:43 +0200

    Fix factory_bot deprecation warnings

--- spec/factories/collected_ink.rb
+++ spec/factories/collected_ink.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :collected_ink do
     user
-    brand_name 'Diamine'
-    line_name ''
-    ink_name 'Marine'
-    kind 'bottle'
-    swabbed true
+    brand_name { 'Diamine' }
+    line_name { '' }
+    ink_name { 'Marine' }
+    kind { 'bottle' }
+    swabbed { true }
     sequence(:comment) { |n| "Dry time: #{n}" }
-    color '#40E0D0'
+    color { '#40E0D0' }
     add_attribute(:private) { false }
   end
 end
