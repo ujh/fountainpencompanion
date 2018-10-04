11243b919263598eec3d74bc4c35c49dafaabae6
Urban Hafner <contact@urbanhafner.com>
2018-10-04 08:19:32 +0200

    Save color into NewInkName as well

--- app/operations/save_collected_ink.rb
+++ app/operations/save_collected_ink.rb
@@ -9,7 +9,7 @@ class SaveCollectedInk
     res = collected_ink.update(collected_ink_params)
     update_ink_brand!
     update_new_ink_name!
-    update_color_of!
+    update_color!
     collected_ink.reload if res # to return the updated color
     res
   end
