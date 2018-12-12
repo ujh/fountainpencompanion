7f2f286d66cfade1a5c0d96ae4648b954c02f495
Urban Hafner <contact@urbanhafner.com>
2018-12-12 08:32:06 +0100

    Display actual counts on the leader board

--- app/models/leader_board.rb
+++ app/models/leader_board.rb
@@ -1,24 +1,30 @@
 class LeaderBoard
   def self.inks
-    base_relation.order("count(*) DESC").limit(10)
+    build
   end
 
   def self.bottles
-    base_relation.where(collected_inks: {kind: "bottle"}).order("count(*) DESC").limit(10)
+    build.where(collected_inks: {kind: "bottle"})
   end
 
   def self.samples
-    base_relation.where(collected_inks: {kind: "sample"}).order("count(*) DESC").limit(10)
+    build.where(collected_inks: {kind: "sample"})
   end
 
   def self.brands
-    select_clause = "users.*,  (select sum(1) OVER () from collected_inks where collected_inks.user_id = users.id group by collected_inks.simplified_brand_name limit 1) as brand_count"
-    base_relation.select(select_clause).order("brand_count DESC").limit(10)
+    build(
+      "(select sum(1) OVER () from collected_inks where collected_inks.user_id = users.id group by collected_inks.ink_brand_id limit 1) as counter"
+    )
+  end
+
+  def self.build(select = "count(*) as counter")
+    select_clause = "users.*, #{select}"
+    base_relation.select(select_clause)
   end
 
   def self.base_relation
     User.joins(:collected_inks).where(collected_inks: {
       archived_on: nil, private: false
-    }).group("users.id")
+    }).group("users.id").order("counter DESC").limit(10)
   end
 end
