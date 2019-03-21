daeb518003d891bf1f298a7d9b7fd82939c0d17a
Urban Hafner <contact@urbanhafner.com>
2019-03-21 15:53:53 +0100

    Do not show empty clusters

--- app/controllers/admins/ink_brands_controller.rb
+++ app/controllers/admins/ink_brands_controller.rb
@@ -1,12 +1,16 @@
 class Admins::InkBrandsController < Admins::BaseController
 
   def index
-    @ink_brands = InkBrand.public.order("ink_brands.popular_name ASC").includes(:collected_inks)
+    @ink_brands = InkBrand.public.order("ink_brands.popular_name ASC")
+      .where("new_ink_names_count > 0")
+      .includes(:collected_inks)
   end
 
   def show
     @ink_brand = InkBrand.find(params[:id])
     @new_ink_names = NewInkName.where(ink_brand_id: params[:id])
-      .order("new_ink_names.popular_line_name, new_ink_names.popular_name ASC").includes(:collected_inks)
+      .where("collected_inks_count > 0")
+      .order("new_ink_names.popular_line_name, new_ink_names.popular_name ASC")
+      .includes(:collected_inks)
   end
 end
