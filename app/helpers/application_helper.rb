17506c28eb5a11a3e9107c0f38aecd834b45d11e
Urban Hafner <contact@urbanhafner.com>
2019-10-22 21:16:17 +0200

    Show place in the leaderboards

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def leaderboard_index(method)
+    index = LeaderBoard.send(method).find_index do |entry|
+      entry[:id] == current_user.id
+    end
+    return index.succ if index
+  end
 end
