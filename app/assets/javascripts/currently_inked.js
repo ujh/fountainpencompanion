f76e6f74e485d5e4ebd4ab5d0b474e0f174d0123
Urban Hafner <contact@urbanhafner.com>
2019-10-24 21:30:57 +0200

    Track clicking on usage record button in clicky

--- app/assets/javascripts/currently_inked.js
+++ app/assets/javascripts/currently_inked.js
@@ -8,5 +8,6 @@ $(function() {
   $('#currently-inked .actions .usage').bind('ajax:complete', function() {
     $(this).find('i').addClass('fa-bookmark-o').removeClass('fa-bookmark')
     $(this).attr("title", "Already recorded usage for today")
+    clicky.log('#usage-record', 'CI usage recorded')
   })
 })
