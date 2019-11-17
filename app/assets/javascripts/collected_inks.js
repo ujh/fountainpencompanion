66fd01ff1770206400dcac3ada9cd4fa15811813
Urban Hafner <contact@urbanhafner.com>
2019-11-17 19:58:17 +0100

    Autocomplete for the collected inks beta form

--- app/assets/javascripts/collected_inks.js
+++ app/assets/javascripts/collected_inks.js
@@ -1,5 +1,28 @@
 $(function() {
-  $('#collected-inks-app').each(function() {
-    renderCollectedInks(this)
-  })
-})
+  $("#collected-inks-app").each(function() {
+    renderCollectedInks(this);
+  });
+
+  $("#collected_ink_brand_name").autocomplete({
+    source: function(request, response) {
+      fetch("/brands?term=" + request.term)
+        .then(function(r) {
+          return r.json();
+        })
+        .then(function(r) {
+          var names = $.map(r.data, function(e) {
+            return e.attributes.popular_name;
+          });
+          response(names);
+        });
+    }
+  });
+
+  $("#collected_ink_line_name").autocomplete({
+    source: "/lines"
+  });
+
+  $("#collected_ink_ink_name").autocomplete({
+    source: "/inks"
+  });
+});
