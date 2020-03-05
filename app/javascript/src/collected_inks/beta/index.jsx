43cf5213158e7e1bf86e1640e0c91f152568cf24
Urban Hafner <contact@urbanhafner.com>
2020-03-05 08:52:09 +0100

    Sorting of the colour column

--- app/javascript/src/collected_inks/beta/index.jsx
+++ app/javascript/src/collected_inks/beta/index.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useMemo } from "react";
 import * as ReactDOM from "react-dom";
 import { useTable, useSortBy } from "react-table";
+import convert from "color-convert";
 import { getRequest } from "src/fetch";
 
 export const renderCollectedInksBeta = el => {
@@ -71,7 +72,7 @@ const CollectedInksBetaTable = ({ data }) => {
         Header: "Color",
         accessor: "attributes.color",
         Cell: () => "",
-        disableSortBy: true
+        sortType: colorSort
       },
       {
         Header: "Swabbed",
@@ -276,4 +277,37 @@ const booleanSort = (rowA, rowB, columnId) => {
   if (rowA.values[columnId] == rowB.values[columnId]) return 0;
   if (rowA.values[columnId] && !rowB.values[columnId]) return 1;
   return -1;
+};
+
+const colorSort = (rowA, rowB, columnId) => {
+  if (!rowA.values[columnId]) return 1;
+  if (!rowB.values[columnId]) return -1;
+  const colorA = hexToSortArray(rowA.values[columnId]);
+  const colorB = hexToSortArray(rowB.values[columnId]);
+  let r = sortByElement(colorA, colorB, 0);
+  if (r) return r;
+  r = sortByElement(colorA, colorB, 1);
+  if (r) return r;
+  return sortByElement(colorA, colorB, 2);
+};
+
+const sortByElement = (arrayA, arrayB, i) => {
+  if (arrayA[i] == arrayB[i]) return 0;
+  return arrayA[i] < arrayB[i] ? -1 : 1;
+};
+
+// See https://www.alanzucconi.com/2015/09/30/colour-sorting/
+const hexToSortArray = hex => {
+  const repetitions = 8;
+  const [r, g, b] = convert.hex.rgb(hex);
+  const lum = Math.sqrt(0.241 * r + 0.691 * g + 0.068 * b);
+  const [h, s, v] = convert.hex.hsv(hex);
+  const h2 = Math.round(h * repetitions);
+  let lum2 = Math.round(lum * repetitions);
+  let v2 = Math.round(v * repetitions);
+  if (h2 % 2 == 1) {
+    v2 = repetitions - v2;
+    lum2 = repetitions - lum2;
+  }
+  return [h2, lum2, v2];
 };
