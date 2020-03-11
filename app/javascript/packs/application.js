3e4a0ed1c46805b6b6f404ddd3ee73e3d0923199
Urban Hafner <contact@urbanhafner.com>
2020-03-11 17:46:01 +0100

    Upgrade some of the polyfills

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,15 +7,8 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-// Needs to come first. http://babeljs.io/docs/usage/polyfill/
-import "babel-polyfill";
-// Promise polyfill. https://github.com/taylorhakes/promise-polyfill
-import Promise from "promise-polyfill";
-// To add to window
-if (!window.Promise) {
-  window.Promise = Promise;
-}
-// fetch polyfill. https://github.com/github/fetch
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 import "whatwg-fetch";
 
 import "../src/color-picker";
