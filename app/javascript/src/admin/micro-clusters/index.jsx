408c6f3fa670ab5b0a3048783a0d25350aad5bad
Urban Hafner <contact@urbanhafner.com>
2020-03-20 22:44:04 +0100

    Group collected inks

--- app/javascript/src/admin/micro-clusters/index.jsx
+++ app/javascript/src/admin/micro-clusters/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useMemo } from "react";
 import ReactDOM from "react-dom";
+import _ from "lodash";
 
 import { getRequest } from "src/fetch";
 
@@ -80,11 +81,15 @@ const DisplayMicroClusters = ({ data }) => {
 };
 
 const DisplayMicroCluster = ({ data }) => {
+  const grouped = _.groupBy(data.collected_inks, ci =>
+    ["brand_name", "line_name", "ink_name"].map(n => ci.attributes[n]).join("")
+  );
   return (
     <div>
       <table className="table table-striped">
         <thead>
           <tr>
+            <th></th>
             <th>{data.attributes.simplified_brand_name}</th>
             <th>{data.attributes.simplified_line_name}</th>
             <th>{data.attributes.simplified_ink_name}</th>
@@ -93,17 +98,25 @@ const DisplayMicroCluster = ({ data }) => {
           </tr>
         </thead>
         <tbody>
-          {data.collected_inks.map(ci => (
-            <tr key={ci.id}>
-              <td>{ci.attributes.brand_name}</td>
-              <td>{ci.attributes.line_name}</td>
-              <td>{ci.attributes.ink_name}</td>
-              <td>{ci.attributes.maker}</td>
-              <td
-                style={{ backgroundColor: ci.attributes.color, width: "30px" }}
-              ></td>
-            </tr>
-          ))}
+          {_.values(grouped).map(cis => {
+            const ci = cis[0];
+            const count = cis.length;
+            return (
+              <tr key={ci.id}>
+                <td>{count}</td>
+                <td>{ci.attributes.brand_name}</td>
+                <td>{ci.attributes.line_name}</td>
+                <td>{ci.attributes.ink_name}</td>
+                <td>{ci.attributes.maker}</td>
+                <td
+                  style={{
+                    backgroundColor: ci.attributes.color,
+                    width: "30px"
+                  }}
+                ></td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </div>
