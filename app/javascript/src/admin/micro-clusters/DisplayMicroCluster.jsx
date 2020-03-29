9bd8cc628fd74c3a7c3f9c383e5da83aafbe3bfb
Urban Hafner <contact@urbanhafner.com>
2020-03-29 16:49:45 +0200

    Separate file for CreateRow component

--- app/javascript/src/admin/micro-clusters/DisplayMicroCluster.jsx
+++ app/javascript/src/admin/micro-clusters/DisplayMicroCluster.jsx
@@ -1,12 +1,9 @@
 import React, { useContext } from "react";
-import _ from "lodash";
 
-import { postRequest } from "src/fetch";
-import { StateContext, DispatchContext } from "./App";
-import { UPDATING, ADD_MACRO_CLUSTER } from "./actions";
+import { StateContext } from "./App";
 import { DisplayMacroClusters } from "./DisplayMacroClusters";
 import { CollectedInksList } from "./CollectedInksList";
-import { assignCluster } from "./assignCluster";
+import { CreateRow } from "./CreateRow";
 
 export const DisplayMicroCluster = ({ afterCreate }) => {
   const { activeCluster } = useContext(StateContext);
@@ -24,72 +21,4 @@ export const DisplayMicroCluster = ({ afterCreate }) => {
       </tbody>
     </table>
   );
-};
-
-const CreateRow = ({ afterCreate }) => {
-  const { updating, activeCluster } = useContext(StateContext);
-  const dispatch = useContext(DispatchContext);
-  const grouped = _.groupBy(activeCluster.collected_inks, ci =>
-    ["brand_name", "line_name", "ink_name"].map(n => ci[n]).join(",")
-  );
-  const ci = _.maxBy(_.values(grouped), array => array.length)[0];
-  const values = {
-    brand_name: ci.brand_name,
-    line_name: ci.line_name,
-    ink_name: ci.ink_name
-  };
-  return (
-    <tr>
-      <th></th>
-      <th>{values.brand_name}</th>
-      <th>{values.line_name}</th>
-      <th>{values.ink_name}</th>
-      <th></th>
-      <th></th>
-      <th></th>
-      <th>
-        <input
-          className="btn btn-default"
-          type="submit"
-          disabled={updating}
-          value="Create"
-          onClick={() => {
-            createMacroClusterAndAssign(
-              values,
-              activeCluster.id,
-              dispatch,
-              afterCreate
-            );
-          }}
-        />
-      </th>
-    </tr>
-  );
-};
-
-const createMacroClusterAndAssign = (
-  values,
-  microClusterId,
-  dispatch,
-  afterCreate
-) => {
-  dispatch({ type: UPDATING });
-  postRequest("/admins/macro_clusters.json", {
-    data: {
-      type: "macro_cluster",
-      attributes: {
-        ...values
-      }
-    }
-  })
-    .then(response => response.json())
-    .then(json =>
-      assignCluster(microClusterId, json.data.id).then(microCluster => {
-        dispatch({
-          type: ADD_MACRO_CLUSTER,
-          payload: microCluster.macro_cluster
-        });
-        afterCreate(microCluster);
-      })
-    );
 };
