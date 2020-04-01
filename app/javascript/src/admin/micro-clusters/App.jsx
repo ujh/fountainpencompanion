5c173567c75226483ce8456d950f228834d702b9
Urban Hafner <contact@urbanhafner.com>
2020-04-01 20:09:32 +0200

    Overlay when updating

--- app/javascript/src/admin/micro-clusters/App.jsx
+++ app/javascript/src/admin/micro-clusters/App.jsx
@@ -27,6 +27,7 @@ export const App = () => {
       <DispatchContext.Provider value={dispatch}>
         <StateContext.Provider value={state}>
           <div>
+            <LoadingOverlay />
             <Summary />
             <BrandSelector />
             <DisplayMicroClusters />
@@ -37,6 +38,21 @@ export const App = () => {
   } else {
     return <Spinner />;
   }
+};
+
+const LoadingOverlay = () => {
+  const { updating } = useContext(StateContext);
+  if (!updating) return null;
+  const style = {
+    position: "fixed",
+    top: 0,
+    left: 0,
+    height: "100%",
+    width: "100%",
+    zIndex: 10,
+    backgroundColor: "rgba(0,0,0,0.5)"
+  };
+  return <div style={style}></div>;
 };
 
 const Summary = () => {
