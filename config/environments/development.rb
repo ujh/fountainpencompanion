1bb08954755e3579c22a4b7daea6e2306e8c2b8b
Urban Hafner <contact@urbanhafner.com>
2020-04-03 18:28:44 +0200

    Remove active_storage configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,9 +28,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
-
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :letter_opener
   # Don't care if the mailer can't send.
