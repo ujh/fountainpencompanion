4e28fc32692713b08b5a19bd7797ade1aadb202c
Urban Hafner <contact@urbanhafner.com>
2020-04-03 13:53:05 +0200

    Fewer clusters per page
    Maybe that will reduce the amount of RAM each process needs

--- app/models/micro_cluster.rb
+++ app/models/micro_cluster.rb
@@ -2,7 +2,7 @@ class MicroCluster < ApplicationRecord
   belongs_to :macro_cluster, optional: true
   has_many :collected_inks
 
-  paginates_per 250
+  paginates_per 100
 
   scope :unassigned, -> { where(macro_cluster_id: nil) }
 end
