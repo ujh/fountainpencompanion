de026ce4a85d2a260fa821e2da7ff0f8bb88d927
Urban Hafner <contact@urbanhafner.com>
2020-05-01 14:12:47 +0200

    Assign all macro clusters with the same brand name at the same time

--- app/controllers/admins/brand_clusters_controller.rb
+++ app/controllers/admins/brand_clusters_controller.rb
@@ -13,7 +13,10 @@ class Admins::BrandClustersController < Admins::BaseController
   def create
     macro_cluster = MacroCluster.find(params[:macro_cluster_id])
     brand_cluster = BrandCluster.create!(name: macro_cluster.brand_name)
-    macro_cluster.update!(brand_cluster: brand_cluster)
+    # Assign all clusters with the name brand name
+    MacroCluster.where(brand_name: macro_cluster.brand_name).update_all(
+      brand_cluster_id: brand_cluster.id
+    )
     flash[:notice] = "Brand cluster '#{brand_cluster.name}' created"
     redirect_to new_admins_brand_cluster_path
   end
@@ -21,7 +24,10 @@ class Admins::BrandClustersController < Admins::BaseController
   def update
     macro_cluster = MacroCluster.find(params[:id])
     brand_cluster = BrandCluster.find(params[:brand_cluster_id])
-    macro_cluster.update!(brand_cluster: brand_cluster)
+    # Assign all clusters with the name brand name
+    MacroCluster.where(brand_name: macro_cluster.brand_name).update_all(
+      brand_cluster_id: brand_cluster.id
+    )
     brand_cluster.update_name!
     redirect_to new_admins_brand_cluster_path
   end
