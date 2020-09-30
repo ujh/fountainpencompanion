915d426b7f6e9b0ca0266e6ca1b7c1470e21c71c
Urban Hafner <contact@urbanhafner.com>
2020-09-30 17:44:26 +0200

    Search field for inks

--- app/controllers/inks_controller.rb
+++ app/controllers/inks_controller.rb
@@ -1,10 +1,23 @@
 class InksController < ApplicationController
   def index
-    clusters = MacroCluster.autocomplete_ink_search(params[:term], params[:brand_name])
-    serializer = MacroClusterSerializer.new(
-      clusters, fields: { macro_cluster: [:ink_name]}
-    )
-    render json: serializer.serializable_hash.to_json
+    respond_to do |format|
+      format.json {
+        clusters = MacroCluster.autocomplete_ink_search(params[:term], params[:brand_name])
+        serializer = MacroClusterSerializer.new(
+          clusters, fields: { macro_cluster: [:ink_name]}
+        )
+        render json: serializer.serializable_hash.to_json
+      }
+      format.html {
+        # These are ordered by rank!
+        mc_ids = CollectedInk.search(params[:q]).where(private: false).joins(
+          micro_cluster: :macro_cluster
+        ).pluck('macro_clusters.id').uniq
+        @clusters = MacroCluster.where(id: mc_ids).sort_by do |mc|
+          mc_ids.index(mc.id)
+        end
+      }
+    end
   end
 
   def show
