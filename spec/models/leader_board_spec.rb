48e064c586c0674fb505e0ec85cd74c93602d44b
Urban Hafner <contact@urbanhafner.com>
2020-10-11 14:27:07 +0200

    Tests for inks_by_popularity method

--- spec/models/leader_board_spec.rb
+++ spec/models/leader_board_spec.rb
@@ -2,7 +2,30 @@ require 'rails_helper'
 
 describe LeaderBoard do
   describe '#inks_by_popularity' do
+    it 'orders by number of collected inks assigned to a macro cluster' do
+      # One micro cluster that has three collected inks
+      macro_cluster1 = create(:macro_cluster)
+      micro_cluster11 = create(:micro_cluster, macro_cluster: macro_cluster1)
+      create_list(:collected_ink, 3, micro_cluster: micro_cluster11)
+      # Two micro clusters with one collected ink each
+      macro_cluster2 = create(:macro_cluster)
+      micro_cluster21 = create(:micro_cluster, macro_cluster: macro_cluster2)
+      micro_cluster22 = create(:micro_cluster, macro_cluster: macro_cluster2)
+      create(:collected_ink, micro_cluster: micro_cluster21)
+      create(:collected_ink, micro_cluster: micro_cluster22)
+      # One micro cluster with one collected ink
+      macro_cluster3 = create(:macro_cluster)
+      micro_cluster31 = create(:micro_cluster, macro_cluster: macro_cluster3)
+      create(:collected_ink, micro_cluster: micro_cluster31)
 
+      expect(described_class.inks_by_popularity.map {|mc| [mc.id, mc.ci_count]}).to eq([[
+        macro_cluster1.id, 3
+      ], [
+        macro_cluster2.id, 2
+      ], [
+        macro_cluster3.id, 1
+      ]])
+    end
   end
 
   describe '#top_inks_by_popularity' do
