38cf9656c8a52c02bc48682a57e67a279ec526bf
Urban Hafner <contact@urbanhafner.com>
2021-01-03 22:29:58 +0100

    Fix N+1 query
    We include the ID of the macro cluster for each ink in the return value
    so we need to include the micro clusters when we query for the collected
    inks.

--- app/serializable/serializable_user.rb
+++ app/serializable/serializable_user.rb
@@ -4,7 +4,7 @@ class SerializableUser < JSONAPI::Serializable::Resource
 
   has_many :collected_inks do
     data do
-      @object.public_inks
+      @object.public_inks.includes(:micro_cluster)
     end
   end
 
