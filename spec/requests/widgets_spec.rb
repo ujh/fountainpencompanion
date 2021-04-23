a31d0d162c3af0b4efc9b5ad445f7c1b83a3b119
Urban Hafner <contact@urbanhafner.com>
2021-04-23 12:19:12 +0200

    Shared authentication example

--- spec/requests/widgets_spec.rb
+++ spec/requests/widgets_spec.rb
@@ -2,18 +2,30 @@ require 'rails_helper'
 
 describe WidgetsController do
   describe '#show' do
-    context "inks_summary" do
+    shared_examples "authentication" do
       it "requires authentication" do
-        get "/dashboard/widgets/inks_summary.json"
+        get url
         expect(response).to have_http_status(:unauthorized)
       end
+    end
+
+    context "inks_summary" do
+      let(:url) { "/dashboard/widgets/inks_summary.json" }
+
+      include_examples "authentication"
 
       it "returns the required data when logged in" do
         user = create(:user)
         sign_in(user)
-        get "/dashboard/widgets/inks_summary.json"
+        get url
         expect(response).to be_successful
       end
+    end
+
+    context 'inks_grouped_by_brand' do
+      let(:url) { "/dashboard/widgets/inks_grouped_by_brand.json" }
+
+      include_examples "authentication"
     end
   end
 end
