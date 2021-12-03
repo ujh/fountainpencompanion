834f22dd783f46285a72328656ebfb0c76758ea7
Urban Hafner <contact@urbanhafner.com>
2021-12-03 14:21:14 +0100

    Using open-uri lead to weird Sidekiq crashes in dev mode

--- app/workers/process_ink_review_submission.rb
+++ app/workers/process_ink_review_submission.rb
@@ -1,12 +1,10 @@
-require 'open-uri'
-
 class ProcessInkReviewSubmission
   include Sidekiq::Worker
 
   def perform(id)
     self.ink_review_submission = InkReviewSubmission.find(id)
     result = unfurl
-    ink_review = InkReview.find_or_create_by(url: result.url, macro_cluster: ink_review_submission.macro_cluster) do |ink_review|
+    ink_review = InkReview.find_or_create_by!(url: result.url, macro_cluster: ink_review_submission.macro_cluster) do |ink_review|
       ink_review.title = unfurl.title
       ink_review.description = unfurl.description
       ink_review.image = unfurl.image
@@ -19,10 +17,14 @@ class ProcessInkReviewSubmission
   attr_accessor :ink_review_submission
 
   def unfurl
-    Unfurler.new(URI.open(url_to_unfurl)).perform
+    Unfurler.new(html).perform
+  end
+
+  def html
+    Net::HTTP.get(url_to_unfurl)
   end
 
   def url_to_unfurl
-    ink_review_submission.url
+    URI(ink_review_submission.url)
   end
 end
