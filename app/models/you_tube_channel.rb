ec6250d6c6693cb87fbd37ec3ffa139bba7b715e
Urban Hafner <contact@urbanhafner.com>
2022-04-10 11:06:29 +0200

    Pick YouTube channels to check for reviews dynamically

--- app/models/you_tube_channel.rb
+++ app/models/you_tube_channel.rb
@@ -1,3 +1,13 @@
 class YouTubeChannel < ApplicationRecord
   has_many :ink_reviews
+
+  def self.channel_ids_for_reviews
+    joins(:ink_reviews).where.not(
+      ink_reviews: { approved_at: nil}
+    ).group(
+      :channel_id
+    ).having(
+      'count(ink_reviews.id) >= 3'
+    ).pluck(:channel_id)
+  end
 end
---
+++ spec/models/you_tube_channel_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+describe YouTubeChannel do
+  describe '#channel_ids_for_reviews' do
+    it 'returns a channel that has at least 3 approved reviews' do
+      channel = create(:you_tube_channel)
+      create_list(:ink_review, 3, you_tube_channel: channel, approved_at: Time.now)
+      expect(described_class.channel_ids_for_reviews).to eq([channel.channel_id])
+    end
+
+    it 'does not return a channel with 2 approved reviews' do
+      channel = create(:you_tube_channel)
+      create_list(:ink_review, 2, you_tube_channel: channel, approved_at: Time.now)
+      expect(described_class.channel_ids_for_reviews).to eq([])
+    end
+
+    it 'does not return a channel with unapproved reviews' do
+      channel = create(:you_tube_channel)
+      create_list(:ink_review, 3, you_tube_channel: channel, approved_at: nil)
+      expect(described_class.channel_ids_for_reviews).to eq([])
+    end
+
+    it 'does not return a channel with rejected reviews' do
+      channel = create(:you_tube_channel)
+      create_list(:ink_review, 3, you_tube_channel: channel, rejected_at: nil)
+      expect(described_class.channel_ids_for_reviews).to eq([])
+    end
+
+    it 'does not return a channel with no reviews' do
+      channel = create(:you_tube_channel)
+      expect(described_class.channel_ids_for_reviews).to eq([])
+    end
+  end
+end
