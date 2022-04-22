9c8bdb3a38509cbc4f641604aeb563b34a045a4a
Urban Hafner <contact@urbanhafner.com>
2022-04-22 10:49:18 +0200

    Tests for the fetch_videos method

--- spec/lib/youtube_spec.rb
+++ spec/lib/youtube_spec.rb
@@ -21,4 +21,38 @@ describe Youtube do
       expect(subject.channel_name).to eq('channel_name')
     end
   end
+
+  describe '#fetch_videos' do
+    let(:client) do
+      double(
+        :client,
+        list_channels: double(
+          items: [double(
+            content_details: double(
+              related_playlists: double(uploads: 'uploads_id')
+            )
+          )]
+        ),
+        list_playlist_items: list_playlist_items
+      )
+    end
+    let(:list_playlist_items) do
+      list_double = double(:list_playlist_items)
+      video1 = double(:video, snippet: double(resource_id: double(video_id: 'video1'), title: 'title1'))
+      video2 = double(:video, snippet: double(resource_id: double(video_id: 'video2'), title: 'title2'))
+      allow(list_double).to receive(:items).and_return([video1], [video2])
+      allow(list_double).to receive(:next_page_token).and_return('token', nil)
+      list_double
+    end
+
+    it 'returns the video info' do
+      expect(subject.fetch_videos.to_a).to eq([{
+        url: "https://youtube.com/watch?v=video1",
+        title: "title1"
+      }, {
+        url: "https://youtube.com/watch?v=video2",
+        title: "title2"
+      }])
+    end
+  end
 end
