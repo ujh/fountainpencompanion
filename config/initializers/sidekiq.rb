bdcc3273e7e62b3a210d0326cb6297cf0aabca0f
Urban Hafner <contact@urbanhafner.com>
2022-06-19 18:05:41 +0200

    Make it work with Heroku premium redis

---
+++ config/initializers/sidekiq.rb
@@ -0,0 +1,8 @@
+# Heroku uses self signed certificates for their premium Redis
+Sidekiq.configure_server do |config|
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+end
