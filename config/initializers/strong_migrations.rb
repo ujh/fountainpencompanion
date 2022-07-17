fd19218e743ffebd9db961bc96ed5e7f5a7698d8
Urban Hafner <contact@urbanhafner.com>
2022-07-17 18:12:51 +0200

    Disable the bot field for now
    For some reason I am getting HTTP parse errors in that bit of code. No
    idea if this will help and they won't appear again or if this will
    continue to happen.
    
    If it happens again, I'll put this all back.

--- config/initializers/strong_migrations.rb
+++ config/initializers/strong_migrations.rb
@@ -1,4 +1,4 @@
 StrongMigrations.start_after = 20200203072140
 StrongMigrations.statement_timeout = 1.hour
 StrongMigrations.lock_timeout = 10.seconds
-StrongMigrations.target_postgresql_version = "12"
+StrongMigrations.target_postgresql_version = "14"
