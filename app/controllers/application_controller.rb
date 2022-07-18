99684fead86efb58e5b6170db69cf09ee5b96999
Urban Hafner <contact@urbanhafner.com>
2022-07-18 08:14:50 +0200

    Re-enable the bot field
    The exception is still there (as expected), so let's put the code back.
    
    I'm now also ignoring the exception as I highly suspect that this is
    due to actual bot users.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  # before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   protect_from_forgery with: :exception
   around_action :set_time_zone
 
@@ -19,9 +19,9 @@ class ApplicationController < ActionController::Base
 
   private
 
-  # def configure_permitted_parameters
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:bot_field])
-  # end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:bot_field])
+  end
 
   def set_time_zone
     if current_user && current_user.time_zone.present?
