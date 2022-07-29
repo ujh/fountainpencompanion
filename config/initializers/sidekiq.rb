3674829002e30709066e8a1fa524d038a7b433f6
Urban Hafner <contact@urbanhafner.com>
2022-07-29 13:43:10 +0200

    Move Sidekiq schedule into a separate config file

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,14 @@
+require 'sidekiq'
+require 'sidekiq-scheduler'
+
+# Load schedule from separate file
+Sidekiq.configure_server do |config|
+  config.on(:startup) do
+    Sidekiq.schedule = YAML.load_file(File.expand_path('../../sidekiq_schedule.yml', __FILE__))
+    SidekiqScheduler::Scheduler.instance.reload_schedule!
+  end
+end
+
 # Heroku uses self signed certificates for their premium Redis
 Sidekiq.configure_server do |config|
   config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
