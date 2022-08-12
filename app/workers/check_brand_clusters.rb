898e4c9984ded0d735d8d643ae37e069ad28a207
Urban Hafner <contact@urbanhafner.com>
2022-08-12 09:50:51 +0200

    Speed up cluster checking
    The overhead was adding a separate job for each cluster to redis. Let's
    do batches of 50 clusters instead and see if that helps.

--- app/workers/check_brand_clusters.rb
+++ app/workers/check_brand_clusters.rb
@@ -1,9 +1,27 @@
 class CheckBrandClusters
   include Sidekiq::Worker
 
-  def perform
-    MacroCluster.find_each do |macro_cluster|
-      CheckBrandCluster.perform_async(macro_cluster.id)
+  def perform(ids = nil)
+    if ids.present?
+      Array(ids).each {|id| check_cluster(id) }
+    else
+      MacroCluster.pluck(:id).in_groups_of(50, false) do |ids|
+        CheckBrandClusters.perform_async(ids)
+      end
     end
+  end
+
+  private
+
+  def check_cluster(id)
+    cluster = MacroCluster.find(id)
+    brand_cluster = cluster.brand_cluster
+    return if brand_cluster.blank?
+    return if cluster.brand_name == brand_cluster.name
+
+    new_brand_cluster = BrandCluster.where.not(
+      id: brand_cluster.id
+    ).where(name: cluster.brand_name).first
+    cluster.update!(brand_cluster: new_brand_cluster) if new_brand_cluster
   end
 end
