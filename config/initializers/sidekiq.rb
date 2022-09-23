a368a8653412ae5e3eaedb7ef41e920bdda8d47a
Urban Hafner <contact@urbanhafner.com>
2022-09-23 13:43:50 +0200

    Clean up after Redis move to DigitalOcean
    * No need for redis could env url
    * No need to bypass SSL security

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,12 +8,3 @@ Sidekiq.configure_server do |config|
     SidekiqScheduler::Scheduler.instance.reload_schedule!
   end
 end
-
-# Heroku uses self signed certificates for their premium Redis
-Sidekiq.configure_server do |config|
-  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
-end
-
-Sidekiq.configure_client do |config|
-  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
-end
