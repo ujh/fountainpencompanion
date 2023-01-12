a1da233a07d39bc7db5743ca07aa1fc817fd9b0a
William Killerud <william@killerud.com>
2023-01-12 21:37:55 +0100

    Have babel transform modules to fix recharts

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -8,4 +8,12 @@ if (!Array.isArray(nodeModulesLoader.exclude)) {
 }
 nodeModulesLoader.exclude.push(/react-table/);
 
+try {
+  nodeModulesLoader.use[0].options.presets[0][1].modules = "commonjs";
+} catch (e) {
+  console.warn(
+    "Webpack config has changed. Ensure @babel/preset-env modules is still set to commonjs!"
+  );
+}
+
 module.exports = environment;
