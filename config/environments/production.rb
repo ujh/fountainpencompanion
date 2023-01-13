95aa722cd6e1702b6288a24ce99093f9a099fe67
Urban Hafner <contact@urbanhafner.com>
2023-01-13 16:07:51 +0100

    Remove middleware from removed gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,8 +99,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.middleware.use ExceptionNotification::Rack
-
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
   # middleware. The `delay` is used to determine how long to wait after a write
