0863e84c465f65705932552f993b369e0e8b0a37
Urban Hafner <contact@urbanhafner.com>
2023-05-17 19:26:01 +0200

    Even an admin goes to the user dashboard if he logs in as a user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    resource.admin? ? admins_dashboard_path : dashboard_path
+    admin_signed_in? ? admins_dashboard_path : dashboard_path
   end
 
   private
