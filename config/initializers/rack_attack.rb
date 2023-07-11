16a621db7316f65d438f3a3eda144a3287b7e82e
Urban Hafner <contact@urbanhafner.com>
2023-07-11 09:18:31 +0200

    Throttle inks full text search some more

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,5 +1,9 @@
-Rack::Attack.throttle("inks limit", limit: 5, period: 2) do |request|
-  if request.path.starts_with?("/brands") || request.path.starts_with?("/inks")
+Rack::Attack.throttle(
+  "full text search limit",
+  limit: 1,
+  period: 3
+) do |request|
+  if request.path.starts_with?("/inks") && request.query_string.include?("q=")
     request.ip
   end
 end
