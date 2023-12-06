b25e4ef1b03715edc2c3d7a810000d013fd79cd8
Urban Hafner <contact@urbanhafner.com>
2023-12-06 18:29:54 +0100

    Shorter statement timeout for migrations

--- config/initializers/strong_migrations.rb
+++ config/initializers/strong_migrations.rb
@@ -1,4 +1,4 @@
 StrongMigrations.start_after = 20_200_203_072_140
-StrongMigrations.statement_timeout = 1.hour
+StrongMigrations.statement_timeout = 5.minutes
 StrongMigrations.lock_timeout = 10.seconds
 StrongMigrations.target_postgresql_version = "14"
