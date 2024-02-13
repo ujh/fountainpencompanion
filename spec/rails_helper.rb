0aa47bc1ce31d480bd9522ec9092fa97a0752f32
Urban Hafner <contact@urbanhafner.com>
2024-02-13 16:26:25 +0100

    Fix deprecation warnings

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -13,7 +13,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_paths = ["#{::Rails.root}/spec/fixtures"]
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
