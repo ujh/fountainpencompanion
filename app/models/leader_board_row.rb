9220b5c4a1fe6d9e2925c7dcd0ae5d8f3ce6023b
Urban Hafner <contact@urbanhafner.com>
2024-02-22 16:28:08 +0100

    Do not use a default scope
    With the default scope in place we cannot delete users with leader board
    rows with a value of zero.

--- app/models/leader_board_row.rb
+++ app/models/leader_board_row.rb
@@ -1,10 +1,9 @@
 class LeaderBoardRow < ApplicationRecord
   belongs_to :user
 
-  default_scope { includes(:user).order(value: :desc).where("value > 0") }
-
   def self.to_leader_board
-    all.map do |row|
+    rows = includes(:user).order(value: :desc).where("value > 0")
+    rows.map do |row|
       {
         id: row.user.id,
         public_name: row.user.public_name,
