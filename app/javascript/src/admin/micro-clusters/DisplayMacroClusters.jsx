59d55e02c77f0bf86e2a8146e7c3e33a0f28e8bd
Urban Hafner <contact@urbanhafner.com>
2024-04-24 18:17:33 +0200

    Generic MacoClusterRows component

--- app/javascript/src/admin/micro-clusters/DisplayMacroClusters.jsx
+++ app/javascript/src/admin/micro-clusters/DisplayMacroClusters.jsx
@@ -1,18 +1,13 @@
 import levenshtein from "fast-levenshtein";
-import _ from "lodash";
-import { matchSorter } from "match-sorter";
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext, useEffect } from "react";
 
-import { MacroClusterRow } from "../components/clustering/MacroClusterRow";
 import {
   NEXT_MACRO_CLUSTER,
   PREVIOUS_MACRO_CLUSTER
 } from "../components/clustering/actions";
-import {
-  keyDownListener,
-  setInBrandSelector
-} from "../components/clustering/keyDownListener";
-import { DispatchContext, StateContext } from "./GenericApp";
+import { keyDownListener } from "../components/clustering/keyDownListener";
+import { DispatchContext } from "./GenericApp";
+import { MacroClusterRows } from "../components/clustering/MacroClusterRows";
 
 export const DisplayMacroClusters = ({ afterAssign, assignCluster }) => {
   const dispatch = useContext(DispatchContext);
@@ -23,85 +18,19 @@ export const DisplayMacroClusters = ({ afterAssign, assignCluster }) => {
     });
   }, [dispatch]);
   return (
-    <MacroClusterRows afterAssign={afterAssign} assignCluster={assignCluster} />
+    <MacroClusterRows
+      afterAssign={afterAssign}
+      assignCluster={assignCluster}
+      fields={["brand_name", "line_name", "ink_name"]}
+      withDistance={withDistance}
+      extraColumn={extraColumn}
+    />
   );
-};
-
-const MacroClusterRows = ({ afterAssign, assignCluster }) => {
-  const {
-    macroClusters,
-    activeCluster,
-    selectedMacroClusterIndex,
-    updateCounter
-  } = useContext(StateContext);
-  const [clustersWithDistance, setClustersWithDistance] = useState([]);
-  const [computing, setComputing] = useState(true);
-  const [search, setSearch] = useState("");
-  useEffect(() => {
-    setComputing(true);
-    setTimeout(() => {
-      setClustersWithDistance(
-        _.sortBy(withDistance(macroClusters, activeCluster), "distance")
-      );
-      setComputing(false);
-    }, 0);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [activeCluster.id, updateCounter]);
-  // Reset search for each new cluster
-  useEffect(() => {
-    setSearch("");
-  }, [activeCluster.id]);
-  if (computing)
-    return (
-      <tr className="loading">
-        <td colSpan="8">Computing ...</td>
-      </tr>
-    );
-  const clustersToRender = search
-    ? matchSorter(clustersWithDistance, search, {
-        keys: ["brand_name", "line_name", "ink_name"]
-      })
-    : clustersWithDistance;
-  const rows = clustersToRender
-    .slice(0, 100) // Shaves of > 1s when rerendering
-    .map((macroCluster, index) => (
-      <MacroClusterRow
-        key={macroCluster.id}
-        macroCluster={macroCluster}
-        afterAssign={afterAssign}
-        assignCluster={assignCluster}
-        selected={index == selectedMacroClusterIndex}
-        fields={["brand_name", "line_name", "ink_name"]}
-        extraColumn={extraColumn}
-      />
-    ));
-  const inputRow = (
-    <tr key="search-box">
-      <td colSpan="8">
-        <input
-          className="form-control"
-          aria-label="Filter"
-          placeholder="Filter existing"
-          type="text"
-          value={search}
-          onChange={(e) => {
-            e.stopPropagation();
-            setSearch(e.target.value);
-          }}
-          onFocus={() => setInBrandSelector(true)}
-          onBlur={() => {
-            setInBrandSelector(false);
-          }}
-        ></input>
-      </td>
-    </tr>
-  );
-  return [inputRow, ...rows];
 };
 
 // This is the most expensive computation in this app. Group inks by name first
 // and only compare between those that are really different.
-const withDistance = (macroClusters, activeCluster) => {
+export const withDistance = (macroClusters, activeCluster) => {
   const activeGroupedInks = activeCluster.grouped_entries;
   return macroClusters.map((c) => {
     const macroClusterInks = c.grouped_entries.concat(c);
