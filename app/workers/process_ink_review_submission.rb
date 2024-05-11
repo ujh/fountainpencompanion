9159167d794e7cee6157cbf9f816edfe3553573e
Urban Hafner <contact@urbanhafner.com>
2024-05-11 11:34:23 +0200

    Do not fail when url can't be access due to a 403 error

--- app/workers/process_ink_review_submission.rb
+++ app/workers/process_ink_review_submission.rb
@@ -5,10 +5,7 @@ class ProcessInkReviewSubmission
     self.ink_review_submission = InkReviewSubmission.find(id)
 
     ink_review =
-      InkReview.find_or_initialize_by(
-        url: url,
-        macro_cluster: macro_cluster
-      ) do |ink_review|
+      InkReview.find_or_initialize_by(url:, macro_cluster:) do |ink_review|
         ink_review.title = title
         ink_review.description = description
         ink_review.image = image
@@ -17,11 +14,11 @@ class ProcessInkReviewSubmission
     if ink_review.save
       ink_review.update(rejected_at: nil)
       ink_review_submission.update(
-        ink_review: ink_review,
+        ink_review:,
         unfurling_errors: nil,
         html: nil
       )
-      ink_review.auto_approve! if (ink_review.ink_review_submissions.size > 1)
+      ink_review.auto_approve! if ink_review.ink_review_submissions.size > 1
     else
       ink_review_submission.update(
         unfurling_errors: ink_review.errors.messages.to_json
@@ -32,7 +29,7 @@ class ProcessInkReviewSubmission
         YouTubeChannel.find_or_create_by(channel_id: you_tube_channel_id)
       ink_review.update!(you_tube_channel: channel)
     end
-  rescue URI::InvalidURIError
+  rescue URI::InvalidURIError, Faraday::ForbiddenError
     ink_review&.destroy
     ink_review_submission&.destroy
   end
