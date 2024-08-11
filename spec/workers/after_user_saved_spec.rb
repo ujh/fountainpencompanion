5956ae4ba71dfb8fe76417ef2117e8505ff59800
Urban Hafner <contact@urbanhafner.com>
2024-08-11 12:05:09 +0200

    Mark users as scheduled for a review
    When a user blurb contains at least one link we set the new flag to
    true, so that they can then get reviewed.

---
+++ spec/workers/after_user_saved_spec.rb
@@ -0,0 +1,31 @@
+require "rails_helper"
+
+describe AfterUserSaved do
+  it "marks user for review if it contains URL" do
+    user = create(:user, blurb: "http://www.ruby.com")
+    described_class.new.perform(user.id)
+    user.reload
+    expect(user.review_blurb).to be true
+  end
+
+  it "marks user if review contains Markdown link" do
+    user = create(:user, blurb: "[Ruby](ruby.com)")
+    described_class.new.perform(user.id)
+    user.reload
+    expect(user.review_blurb).to be true
+  end
+
+  it "marks user for review if it contains URL without protocol" do
+    user = create(:user, blurb: "www.ruby.com")
+    described_class.new.perform(user.id)
+    user.reload
+    expect(user.review_blurb).to be true
+  end
+
+  it "marks user as not for review if it does not contain a link" do
+    user = create(:user, blurb: "random text")
+    described_class.new.perform(user.id)
+    user.reload
+    expect(user.review_blurb).to be false
+  end
+end
