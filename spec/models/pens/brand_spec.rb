8b8b7c8d00010daddc9ce728b12022b4efdff1d4
Urban Hafner <contact@urbanhafner.com>
2024-08-24 12:44:17 +0200

    Fix spec after functional change

--- spec/models/pens/brand_spec.rb
+++ spec/models/pens/brand_spec.rb
@@ -10,12 +10,19 @@ describe Pens::Brand do
 
   describe "#names" do
     it "returns all the unique names of all assigned models" do
-      brand = create(:pens_brand)
+      brand = create(:pens_brand, name: "Brand 1")
       create(:pens_model, pen_brand: brand, brand: "Brand 1")
       create(:pens_model, pen_brand: brand, brand: "Brand 1")
       create(:pens_model, pen_brand: brand, brand: "Brand 2")
 
       expect(brand.names).to match_array(["Brand 1", "Brand 2"])
+    end
+
+    it "includes the brand name if it is different" do
+      brand = create(:pens_brand, name: "Brand X")
+      create(:pens_model, pen_brand: brand, brand: "Brand 1")
+
+      expect(brand.names).to match_array(["Brand 1", "Brand X"])
     end
   end
 
