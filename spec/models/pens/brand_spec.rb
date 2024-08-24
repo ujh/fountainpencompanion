05a64daa6dddd69d3fbe8a50ab8c91f3dc4a1da9
Urban Hafner <contact@urbanhafner.com>
2024-08-24 12:32:04 +0200

    Specs for Pens::Brand methods

--- spec/models/pens/brand_spec.rb
+++ spec/models/pens/brand_spec.rb
@@ -7,4 +7,26 @@ describe Pens::Brand do
 
     expect(brand.models).to eq([model])
   end
+
+  describe "#names" do
+    it "returns all the unique names of all assigned models" do
+      brand = create(:pens_brand)
+      create(:pens_model, pen_brand: brand, brand: "Brand 1")
+      create(:pens_model, pen_brand: brand, brand: "Brand 1")
+      create(:pens_model, pen_brand: brand, brand: "Brand 2")
+
+      expect(brand.names).to match_array(["Brand 1", "Brand 2"])
+    end
+  end
+
+  describe "#synonyms" do
+    it "returns all unique names of all assigned models, apart from the brand's own name" do
+      brand = create(:pens_brand, name: "Brand")
+      create(:pens_model, pen_brand: brand, brand: "Brand")
+      create(:pens_model, pen_brand: brand, brand: "Brand 1")
+      create(:pens_model, pen_brand: brand, brand: "Brand 2")
+
+      expect(brand.synonyms).to match_array(["Brand 1", "Brand 2"])
+    end
+  end
 end
