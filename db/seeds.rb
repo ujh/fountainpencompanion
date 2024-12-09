c6232123151a2f3dc96731d6d2ade6ce0ca48d4c
Urban Hafner <contact@urbanhafner.com>
2024-12-09 13:36:32 +0100

    Better setup with Docker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
-unless User.find_by(email: "urban@bettong.net")
-  user = User.new(email: "urban@bettong.net")
-  user.password = user.password_confirmation = SecureRandom.hex
-  user.admin = true
-  user.save!
-end
+return unless Rails.env.development?
+
+user = User.find_or_initialize_by(email: "urban@bettong.net")
+user.password = user.password_confirmation = "password"
+user.admin = true
+user.confirmed_at = Time.now
+user.save
