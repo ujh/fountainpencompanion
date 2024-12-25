6420f58b1cad470c3c9ab4256fb12e9bc6bc019e
Urban Hafner <contact@urbanhafner.com>
2024-12-25 12:05:04 +0100

    Prepare for adding a production build
    Turn the dev build into a stage and use this in the docker compose file. This
    can then serve as the base for a leaner production build.

---
+++ config/docker/dev-entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -ex
+
+cd /app
+
+
+bundle check || bundle install || true
+
+if [[ $@ =~ 'yarn' ]]
+then
+  yarn install --frozen-lockfile
+else
+  # Remove a potentially pre-existing server.pid for Rails.
+  rm -rf /app/tmp/pids/server.pid
+fi
+
+# Since we specified both an ENTRYPOINT and a CMD (which could be overwritten
+# by a "command"), Docker passes the CMD/command to the ENTRYPOINT script as
+# arguments.  So we can run the CMD/command with exec "$@".
+exec "$@"
