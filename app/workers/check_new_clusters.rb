b23a19014508275ff87edca24cb7f8c2f540d54b
Urban Hafner <contact@urbanhafner.com>
2024-12-30 22:15:26 +0100

    Fix new clusters email sending

--- app/workers/check_new_clusters.rb
+++ app/workers/check_new_clusters.rb
@@ -2,8 +2,10 @@ class CheckNewClusters
   include Sidekiq::Worker
 
   def perform
-    new_clusters = Rails.cache.read("new_cluster_count", raw: true)
-    return unless new_clusters.to_i.positive?
+    # Raw returns a string, but we need a number for the decrement below to work
+    # and not do an increment instead.
+    new_clusters = Rails.cache.read("new_cluster_count", raw: true).to_i
+    return unless new_clusters.positive?
 
     AdminMailer.new_cluster(new_clusters).deliver_later
     Rails.cache.decrement("new_cluster_count", new_clusters)
