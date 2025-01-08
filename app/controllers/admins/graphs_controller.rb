c8010357d24335f3b22bf6e143f2b33d4abb50da
Urban Hafner <contact@urbanhafner.com>
2025-01-08 17:22:44 +0100

    Show more user agents

--- app/controllers/admins/graphs_controller.rb
+++ app/controllers/admins/graphs_controller.rb
@@ -92,7 +92,7 @@ class Admins::GraphsController < Admins::BaseController
               .map { |d| [d.first.to_i * 1000, d.last] }
         }
       end
-      .reject { |data| data[:data].map(&:last).all? { |count| count <= 30 } }
+      .reject { |data| data[:data].map(&:last).all? { |count| count <= 10 } }
   end
 
   def collected_inks
