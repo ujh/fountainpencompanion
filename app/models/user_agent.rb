a799c745f766b9aa1cbc68943238921d563f816d
Urban Hafner <contact@urbanhafner.com>
2025-01-08 17:37:48 +0100

    Ignore more stuff

--- app/models/user_agent.rb
+++ app/models/user_agent.rb
@@ -1,16 +1,21 @@
 class UserAgent < ApplicationRecord
   BROWSERS = [
+    "Avast Secure Browser",
     "Chrome Mobile WebView",
     "Chrome Mobile iOS",
     "Chrome Mobile",
     "Chrome",
     "Consul Health Check",
+    "Ecosia iOS",
     "Edge",
     "Firefox Mobile",
+    "Firefox iOS",
     "Firefox",
+    "Instagram",
     "Mobile Safari",
     "Opera",
-    "Safari"
+    "Safari",
+    "Samsung Internet"
   ].freeze
 
   scope :non_browser, -> { where.not(name: BROWSERS) }
--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -14,7 +14,7 @@ Rack::Attack.throttle(
   period: 20
 ) { |request| request.ip if request.path.starts_with?("/descriptions/missing") }
 
-Rack::Attack.throttle("Google", limit: 1, period: 1) do |request|
+Rack::Attack.throttle("Google", limit: 1, period: 5) do |request|
   "Google" if request.user_agent =~ /Googlebot/i
 end
 
@@ -29,5 +29,5 @@ end
 # See https://social.treehouse.systems/@dee/112524729369220652
 Rack::Attack.blocklist("Misbehaving bots") do |request|
   request.user_agent =~
-    /AhrefsBot|Baiduspider|SemrushBot|SeekportBot|BLEXBot|Buck|magpie-crawler|ZoominfoBot|HeadlessChrome|istellabot|Sogou|coccocbot|Pinterestbot|moatbot|Mediatoolkitbot|SeznamBot|trendictionbot|MJ12bot|DotBot|PetalBot|YandexBot|bingbot|ClaudeBot|imagesift|GPTBot|Bytespider|Timpibot|meta-externalagent/i
+    /AhrefsBot|Baiduspider|SemrushBot|SeekportBot|BLEXBot|Buck|magpie-crawler|ZoominfoBot|HeadlessChrome|istellabot|Sogou|coccocbot|Pinterestbot|moatbot|Mediatoolkitbot|SeznamBot|trendictionbot|MJ12bot|DotBot|PetalBot|YandexBot|bingbot|ClaudeBot|imagesift|GPTBot|Bytespider|Timpibot|meta-externalagent|facebook/i
 end
