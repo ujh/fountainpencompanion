4492fc569794e4a50e05950ac69be44760047609
Urban Hafner <contact@urbanhafner.com>
2025-01-08 17:02:57 +0100

    Hide browsers from user agent graph

--- app/models/user_agent.rb
+++ app/models/user_agent.rb
@@ -1,2 +1,17 @@
 class UserAgent < ApplicationRecord
+  BROWSERS = [
+    "Chrome Mobile WebView",
+    "Chrome Mobile iOS",
+    "Chrome Mobile",
+    "Chrome",
+    "Consul Health Check",
+    "Edge",
+    "Firefox Mobile",
+    "Firefox",
+    "Mobile Safari",
+    "Opera",
+    "Safari"
+  ].freeze
+
+  scope :non_browser, -> { where.not(name: BROWSERS) }
 end
