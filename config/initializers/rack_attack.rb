54fe7bd406a30cc48e31e3ec0613b05fd2e2b5fc
Urban Hafner <contact@urbanhafner.com>
2025-01-08 14:06:31 +0100

    Keep out meta

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -15,19 +15,19 @@ Rack::Attack.throttle(
 ) { |request| request.ip if request.path.starts_with?("/descriptions/missing") }
 
 Rack::Attack.throttle("Google", limit: 1, period: 1) do |request|
-  request.user_agent =~ /Googlebot/i
+  "Google" if request.user_agent =~ /Googlebot/i
 end
 
 Rack::Attack.throttle("crawler", limit: 1, period: 1) do |request|
-  request.user_agent =~ /crawler/i
+  "crawler" if request.user_agent =~ /crawler/i
 end
 
 Rack::Attack.throttle("Amazonbot", limit: 1, period: 10) do |request|
-  request.user_agent =~ /Amazonbot/i
+  "Amazonbot" if request.user_agent =~ /Amazonbot/i
 end
 
 # See https://social.treehouse.systems/@dee/112524729369220652
 Rack::Attack.blocklist("Misbehaving bots") do |request|
   request.user_agent =~
-    /AhrefsBot|Baiduspider|SemrushBot|SeekportBot|BLEXBot|Buck|magpie-crawler|ZoominfoBot|HeadlessChrome|istellabot|Sogou|coccocbot|Pinterestbot|moatbot|Mediatoolkitbot|SeznamBot|trendictionbot|MJ12bot|DotBot|PetalBot|YandexBot|bingbot|ClaudeBot|imagesift|GPTBot|Bytespider|Timpibot/i
+    /AhrefsBot|Baiduspider|SemrushBot|SeekportBot|BLEXBot|Buck|magpie-crawler|ZoominfoBot|HeadlessChrome|istellabot|Sogou|coccocbot|Pinterestbot|moatbot|Mediatoolkitbot|SeznamBot|trendictionbot|MJ12bot|DotBot|PetalBot|YandexBot|bingbot|ClaudeBot|imagesift|GPTBot|Bytespider|Timpibot|meta-externalagent/i
 end
