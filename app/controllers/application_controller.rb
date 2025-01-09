312dd37c0e571fceb4d9e6ca40ae7d9bc5a0b843
Urban Hafner <contact@urbanhafner.com>
2025-01-09 14:42:06 +0100

    Ensure that my patterns have precedence

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,17 @@ class ApplicationController < ActionController::Base
   around_action :set_time_zone
   before_action :save_user_agent
 
-  if !Rails.env.development?
-    rescue_from ActionView::MissingTemplate do |exception|
+  unless Rails.env.development?
+    rescue_from ActionView::MissingTemplate do |_exception|
       render file: "public/404.html", status: :not_found, layout: false
     end
   end
 
   private
 
-  def set_time_zone
+  def set_time_zone(&)
     if current_user && current_user.time_zone.present?
-      Time.use_zone(current_user.time_zone) { yield }
+      Time.use_zone(current_user.time_zone, &)
     else
       yield
     end
@@ -22,8 +22,8 @@ class ApplicationController < ActionController::Base
   USER_AGENT_PARSER =
     UserAgentParser::Parser.new(
       patterns_paths: [
-        UserAgentParser::DefaultPatternsPath,
-        Rails.root.join("config/user_agents.yml")
+        Rails.root.join("config/user_agents.yml"),
+        UserAgentParser::DefaultPatternsPath
       ]
     )
 
