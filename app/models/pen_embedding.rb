cfd6ae246a832635d3a5052f27bc417e367ae8ef
Urban Hafner <contact@urbanhafner.com>
2025-02-06 12:28:34 +0100

    Fetch embeddings automatically

--- app/models/pen_embedding.rb
+++ app/models/pen_embedding.rb
@@ -2,4 +2,14 @@ class PenEmbedding < ApplicationRecord
   has_neighbors :embedding
 
   belongs_to :owner, polymorphic: true
+
+  after_save :fetch_embedding
+
+  private
+
+  def fetch_embedding
+    return unless content_previously_changed?
+
+    FetchEmbedding.perform_async(self.class.name, id)
+  end
 end
--- spec/models/pen_embedding_spec.rb
+++ spec/models/pen_embedding_spec.rb
@@ -1,5 +1,26 @@
 require "rails_helper"
 
-RSpec.describe PenEmbedding, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe PenEmbedding do
+  let(:owner) { create(:collected_pen) }
+
+  it "fetches the embedding when creating the model" do
+    expect do
+      described_class.create!(content: "content", owner: owner)
+    end.to change { FetchEmbedding.jobs.count }.by(1)
+  end
+
+  it "fetches the embedding when content has changed" do
+    embedding = described_class.create!(content: "content", owner: owner)
+    expect do embedding.update!(content: "new content") end.to change {
+      FetchEmbedding.jobs.count
+    }.by(1)
+  end
+
+  it "does not fetch the embedding when model has not changed" do
+    embedding = described_class.create!(content: "content", owner: owner)
+
+    expect { embedding.update!(owner: create(:collected_pen)) }.not_to(
+      change { FetchEmbedding.jobs.count }
+    )
+  end
 end
