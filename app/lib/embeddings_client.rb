b7167ddd2723560342acad2cb78f29c8b39457a2
Urban Hafner <contact@urbanhafner.com>
2025-02-07 15:59:10 +0100

    Embeddings client

---
+++ app/lib/embeddings_client.rb
@@ -0,0 +1,31 @@
+class EmbeddingsClient
+  def fetch(text)
+    self.text = text
+    fetch_embedding
+  end
+
+  private
+
+  attr_accessor :text
+
+  def fetch_embedding
+    Rails
+      .cache
+      .fetch("embedding:#{digest}") do
+        response = client.embeddings(parameters: { model: "text-embedding-3-small", input: text })
+        response.dig("data", 0, "embedding")
+      end
+  end
+
+  def digest
+    Digest::MD5.hexdigest(text)
+  end
+
+  def client
+    @client ||= OpenAI::Client.new(access_token:, log_errors: !Rails.env.production?)
+  end
+
+  def access_token
+    Rails.env.development? ? ENV.fetch("OPEN_AI_DEV_TOKEN", nil) : ENV.fetch("OPEN_AI_EMBEDDINGS")
+  end
+end
