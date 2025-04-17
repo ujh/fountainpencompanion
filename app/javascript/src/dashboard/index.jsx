04fe89d84ed1a4e40def39b13c9b26edd764e9bb
Urban Hafner <contact@urbanhafner.com>
2025-04-17 13:31:45 +0200

    Update index.jsx

--- app/javascript/src/dashboard/index.jsx
+++ app/javascript/src/dashboard/index.jsx
@@ -8,7 +8,6 @@ import { PensSummaryWidget } from "./pens_summary_widget";
 import { InksVisualizationWidget } from "./inks_visualization_widget";
 import { createRoot } from "react-dom/client";
 import { PenAndInkSuggestionWidget } from "./pen_and_ink_suggestion_widget";
-import { SponsorWidget } from "./sponsor_widget";
 
 document.addEventListener("DOMContentLoaded", () => {
   const el = document.getElementById("dashboard");
