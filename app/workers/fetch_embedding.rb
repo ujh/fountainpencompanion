ed78055f08a67e44861a0ccc8baff7851c5685f3
Urban Hafner <contact@urbanhafner.com>
2025-04-27 12:53:33 +0200

    Higher concurrency for the fetching of embeddings

--- app/workers/fetch_embedding.rb
+++ app/workers/fetch_embedding.rb
@@ -2,7 +2,7 @@ class FetchEmbedding
   include Sidekiq::Worker
   include Sidekiq::Throttled::Worker
 
-  sidekiq_throttle concurrency: { limit: 2 }
+  sidekiq_throttle concurrency: { limit: 4 }
   sidekiq_options queue: "low"
 
   def perform(class_name, id)
