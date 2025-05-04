64b5cd342eb9fafc08347438f62073bfe3c07ff2
Urban Hafner <contact@urbanhafner.com>
2025-05-04 10:00:28 +0200

    Filter by agent log types

--- app/controllers/admins/agent_logs_controller.rb
+++ app/controllers/admins/agent_logs_controller.rb
@@ -1,5 +1,14 @@
 class Admins::AgentLogsController < Admins::BaseController
   def index
-    @agent_logs = AgentLog.order("created_at desc").page(params[:page]).per(1)
+    relation = AgentLog.order("created_at desc")
+    relation = relation.where(name: params[:name]) if params[:name].present?
+    @agent_logs = relation.page(params[:page]).per(1)
+    @agent_log_names = agent_log_names
+  end
+
+  private
+
+  def agent_log_names
+    AgentLog.group(:name).count.map { |k, v| ["#{k} (#{v})", k] }.to_h
   end
 end
