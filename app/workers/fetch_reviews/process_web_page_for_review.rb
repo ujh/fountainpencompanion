433442bcf28957674c409fe582580e1ae04b4df4
Urban Hafner <contact@urbanhafner.com>
2025-05-08 12:10:53 +0200

    Fix bug

--- app/workers/fetch_reviews/process_web_page_for_review.rb
+++ app/workers/fetch_reviews/process_web_page_for_review.rb
@@ -4,15 +4,9 @@ class FetchReviews
     sidekiq_options queue: "reviews"
 
     def perform(page_id)
-      self.web_page_for_review = WebPageForReview.find(page_id)
-      return unless macro_cluster
-
+      web_page_for_review = WebPageForReview.find(page_id)
       ReviewFinder.new(web_page_for_review).perform
       web_page_for_review.update!(state: "processed")
     end
-
-    private
-
-    attr_accessor :web_page_for_review
   end
 end
