62bf325009501f91168a3f0afb3fb261af77c28d
Urban Hafner <contact@urbanhafner.com>
2025-05-12 15:05:15 +0200

    Stats should only take manually processed agent logs into account

--- app/models/agent_log.rb
+++ app/models/agent_log.rb
@@ -2,10 +2,6 @@ class AgentLog < ApplicationRecord
   belongs_to :owner, polymorphic: true, optional: true
   has_many :agent_logs, as: :owner, dependent: :destroy
 
-  scope :ink_clusterer, -> { where(name: "InkClusterer") }
-  scope :manually_processed, -> { processed.where(agent_approved: false) }
-  scope :agent_processed, -> { processed.where(agent_approved: true) }
-
   STATES = [
     PROCESSING = "processing",
     WAITING_FOR_APPROVAL = "waiting-for-approval",
@@ -15,11 +11,15 @@ class AgentLog < ApplicationRecord
 
   validates :state, inclusion: { in: STATES }
 
+  scope :ink_clusterer, -> { where(name: "InkClusterer") }
+
   scope :processing, -> { where(state: PROCESSING) }
   scope :waiting_for_approval, -> { where(state: WAITING_FOR_APPROVAL) }
   scope :approved, -> { where(state: APPROVED) }
   scope :rejected, -> { where(state: REJECTED) }
   scope :processed, -> { where(state: [APPROVED, REJECTED]) }
+  scope :manually_processed, -> { processed.where(agent_approved: false) }
+  scope :agent_processed, -> { processed.where(agent_approved: true) }
 
   def waiting_for_approval!
     update!(state: WAITING_FOR_APPROVAL)
