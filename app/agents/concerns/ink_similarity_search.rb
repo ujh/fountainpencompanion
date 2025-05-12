b430ffa84001e6e5eeeec00000946e61605fb16f
Urban Hafner <contact@urbanhafner.com>
2025-05-12 14:44:48 +0200

    Add fallback in search

--- app/agents/concerns/ink_similarity_search.rb
+++ app/agents/concerns/ink_similarity_search.rb
@@ -7,11 +7,11 @@ module InkSimilaritySearch
 
   included do
     function :ink_similarity_search,
-             "Find the 50 most similar ink clusters by cosine distance",
+             "Find the 20 most similar ink clusters by cosine distance",
              search_string: {
                type: "string"
              } do |arguments|
-      similar_clusters = MacroCluster.embedding_search(arguments[:search_string]).take(50)
+      similar_clusters = MacroCluster.embedding_search(arguments[:search_string]).take(20)
       similar_clusters.map do |data|
         cluster = data.cluster
         data = {
@@ -20,6 +20,19 @@ module InkSimilaritySearch
           distance: data.distance,
           synonyms: cluster.synonyms
         }
+        data[:color] = cluster.color if cluster.color.present?
+        data
+      end
+    end
+
+    fuction :ink_full_text_search,
+            "Fallback search, when results using similarity search inconclusive. Finds inks by full text search",
+            search_string: {
+              type: "string"
+            } do |arguments|
+      similar_clusters = MacroCluster.full_text_search(arguments[:search_string])
+      similar_clusters.map do |cluster|
+        data = { id: cluster.id, name: cluster.name, synonyms: cluster.synonyms }
         data[:color] = cluster.color if cluster.color.present?
         data
       end
