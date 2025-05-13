d5bf2c8c44ab5f02ff77118c6d5eb147b3a5bafa
Urban Hafner <contact@urbanhafner.com>
2025-05-13 07:07:08 +0200

    Ink clusterer review controller adjustments for agent approvals
    Now does the right thing when reviewing agent approved clusterings.

--- app/controllers/admins/agents/ink_clusterer_controller.rb
+++ app/controllers/admins/agents/ink_clusterer_controller.rb
@@ -11,17 +11,36 @@ class Admins::Agents::InkClustererController < Admins::BaseController
   end
 
   def update
-    InkClusterer.new(micro_cluster.id).approve!
+    if agent_log.agent_approved?
+      if agent_log.approved?
+        # If agent approved it correctly, we just need to change to manually
+        # approved.
+        agent_log.approve!
+      else
+        # If agent rejected it, a follow up action was already taken. We
+        # therefore can only change to manually approved. No further action can
+        # be taken.
+        agent_lot.approve!
+      end
+    else
+      InkClusterer.new(micro_cluster.id).approve!
+    end
     redirect_to admins_agents_ink_clusterer_index_path(page: params[:page])
   end
 
   def destroy
-    clusters_to_reprocess = InkClusterer.new(micro_cluster.id).reject!
-    clusters_to_reprocess.each do |cluster|
-      # Generate a new agent log for the rejected micro cluster
-      InkClusterer.new(cluster.id)
-      # Now schedule the actual ink clustering job
-      RunInkClustererAgent.perform_async("InkClusterer", cluster.id)
+    if agent_log.agent_approved?
+      if agent_log.approved?
+        # If agent approved it, but it needed to be rejected we need to clean
+        # up and reprocess.
+        reject_and_reprocess!
+      else
+        # If agent rejected, but should have approved, a follow up action was
+        # already taken. So we should only change to manually rejected.
+        agent_log.reject!
+      end
+    else
+      reject_and_reprocess!
     end
     redirect_to admins_agents_ink_clusterer_index_path(page: params[:page])
   end
@@ -42,5 +61,15 @@ class Admins::Agents::InkClustererController < Admins::BaseController
       .where(state: [AgentLog::WAITING_FOR_APPROVAL, AgentLog::PROCESSING])
       .or(AgentLog.ink_clusterer.agent_processed)
       .order(:id)
+  end
+
+  def reject_and_reprocess!
+    clusters_to_reprocess = InkClusterer.new(micro_cluster.id).reject!
+    clusters_to_reprocess.each do |cluster|
+      # Generate a new agent log for the rejected micro cluster
+      InkClusterer.new(cluster.id)
+      # Now schedule the actual ink clustering job
+      RunInkClustererAgent.perform_async("InkClusterer", cluster.id)
+    end
   end
 end
