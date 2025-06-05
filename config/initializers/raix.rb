48c62dbb3fcbd57dd2c5848d52a4c21ab061d251
Urban Hafner <contact@urbanhafner.com>
2025-06-05 07:26:57 +0200

    More tool calls by default

--- config/initializers/raix.rb
+++ config/initializers/raix.rb
@@ -2,6 +2,7 @@ access_token =
   Rails.env.development? ? ENV.fetch("OPEN_AI_DEV_TOKEN", nil) : ENV.fetch("OPEN_AI_TOKEN", nil)
 
 Raix.configure do |config|
+  config.max_tool_calls = 50
   config.openai_client =
     OpenAI::Client.new(access_token:) do |f|
       f.response :logger, Logger.new($stdout), bodies: true if Rails.env.development?
