33b12383bbccfa65e2d0f019c5216e7cc283832b
Urban Hafner <contact@urbanhafner.com>
2025-11-02 11:44:33 +0100

    Pick second most popular value if first one already taken

--- app/workers/pens/update_model.rb
+++ app/workers/pens/update_model.rb
@@ -16,22 +16,37 @@ module Pens
     attr_accessor :model
 
     def update_attributes!
-      %i[brand model].each { |attr| model.send("#{attr}=", best_attr_value(attr)) }
+      model.brand = best_attr_value(:brand)
+      begin
+        retried = false
+        model.model = best_attr_value(:model, retried: retried)
+      rescue ActiveRecord::RecordNotUnique
+        raise if retried
+
+        retried = true
+        retry
+      end
       model.save
     end
 
-    def best_attr_value(attr)
+    def best_attr_value(attr, retried: false)
       attr_values = model.collected_pens.map { |cp| cp.send(attr) }.find_all { |v| v.present? }
       return "" if attr_values.empty?
-      # From all values with the most members, pick the one with the longest,
-      # i.e most specific, name.
-      attr_values
-        .tally
-        .group_by(&:last)
-        .max_by { |k, v| k }
-        .last
-        .max_by { |k, v| k.length }
-        .first || ""
+
+      if retried
+        # Pick the second most popular value
+        attr_values.tally.group_by(&:last).sort_by { |k, v| k }.reverse.second.flatten.second
+      else
+        # From all values with the most members, pick the one with the longest,
+        # i.e most specific, name.
+        attr_values
+          .tally
+          .group_by(&:last)
+          .max_by { |k, v| k }
+          .last
+          .max_by { |k, v| k.length }
+          .first || ""
+      end
     end
 
     def update_embedding!
