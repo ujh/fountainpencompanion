2ac98f5885a7bdb58550b2a78c17b46f0fd865da
Urban Hafner <contact@urbanhafner.com>
2025-11-21 08:15:26 +0100

    Worker to refresh all ink clusters

--- app/workers/update_macro_cluster.rb
+++ app/workers/update_macro_cluster.rb
@@ -1,5 +1,8 @@
 class UpdateMacroCluster
   include Sidekiq::Worker
+  include Sidekiq::Throttled::Worker
+
+  sidekiq_throttle concurrency: { limit: 3 }
 
   def perform(id)
     # Load public collected inks for tag calculation
